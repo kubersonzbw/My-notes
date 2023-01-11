876aa3ab746b3a4082e716a55176468010ae003d
Robert <robert.kubera96@gmail.com>
2023-01-11 22:24:38 +0100

    first my-notes

---
+++ main.js
@@ -0,0 +1,90 @@
+import { templateTask } from "../_template";
+
+const addBtn = document.querySelector(".add");
+const saveBtn = document.querySelector(".save");
+const cancelBtn = document.querySelector(".cancel");
+
+const deleteAllBtn = document.querySelector(".delete-all");
+
+const noteArea = document.querySelector(".note-area");
+const notePanel = document.querySelector(".note-panel");
+const category = document.querySelector("#category");
+const textarea = document.querySelector("#text");
+const error = document.querySelector(".error");
+
+let selectedValue;
+let cardID = 0;
+
+console.log(textarea.textContent);
+const openPanel = () => {
+  notePanel.style.display = "flex";
+};
+
+const closePanel = () => {
+  notePanel.style.display = "none";
+  error.style.visibility = "hidden";
+  textarea.value = "";
+  category.selectedIndex = 0;
+};
+
+const addNote = () => {
+  if (
+    textarea.value !== "" &&
+    category.options[category.selectedIndex].value !== "0"
+  ) {
+    createNote();
+    error.style.visibility = "hidden";
+  } else {
+    error.style.visibility = "visible";
+  }
+};
+
+const createNote = () => {
+  const newNote = document.createElement("div");
+  newNote.classList.add("note");
+  newNote.setAttribute("id", cardID);
+
+  noteArea.appendChild(newNote);
+  newNote.insertAdjacentHTML(
+    "beforeend",
+    templateTask(textarea, selectedValue, cardID)
+  );
+  checkColor(newNote);
+  closePanel();
+  const deleteButton = newNote.querySelector(".delete-note");
+  deleteButton.addEventListener("click", () => deleteNote(newNote));
+  cardID++;
+};
+
+const selectValue = () => {
+  selectedValue = category.options[category.selectedIndex].text;
+};
+
+const checkColor = (note) => {
+  switch (selectedValue) {
+    case "Zakupy":
+      note.style.backgroundColor = "rgb(72,255,0)";
+      break;
+    case "Praca":
+      note.style.backgroundColor = "rgb(255,243,0)";
+      break;
+    case "Inne":
+      note.style.backgroundColor = "rgb(0,170,255)";
+      break;
+  }
+};
+
+const deleteNote = (noteToDelete) => {
+  noteArea.removeChild(noteToDelete);
+};
+
+const deleteAll = () => {
+  const allNotes = document.querySelectorAll(".note");
+  allNotes.forEach((e) => e.remove());
+};
+
+deleteAllBtn.addEventListener("click", deleteAll);
+category.addEventListener("change", selectValue);
+saveBtn.addEventListener("click", addNote);
+cancelBtn.addEventListener("click", closePanel);
+addBtn.addEventListener("click", openPanel);
